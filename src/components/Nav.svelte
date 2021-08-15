f740308cefe95bf08f9a3efbb183921045d684c2
Slav Basharov <slavbacharov@gmail.com>
2021-08-15 15:00:02 +0200

    Pointer fix

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -173,7 +173,7 @@
     position: absolute;
     top: 50px;
     left: 20px;
-    min-width: 160px;
+    min-width: 115px;
     display: flex;
     flex-direction: column;
     animation: bounce 1s alternate-reverse 1s infinite ease-in-out;
