6e616defa074cb3628b7a3c9058ffb3ede8bf3b1
Slav Basharov <slavbacharov@gmail.com>
2022-06-04 21:02:16 +0200

    Updated adapter to @sveltejs/adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // svelte.config.js
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 
 const config = {
